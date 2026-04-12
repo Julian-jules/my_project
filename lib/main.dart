1304a1a25631f4e1c7bf35a703fee6ee5ee56108
Julian-jules <julianadhiambo340@gmail.com>
2026-04-12 23:56:01 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:myproject/config/routes.dart';
+import 'package:myproject/views/homescreen.dart';
 import 'package:myproject/views/login.dart';
-import 'package:myproject/views/browse.dart';
-import 'package:myproject/views/favorites.dart';
-import 'package:myproject/views/profile.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+
+  final box = GetStorage();
+  final isLoggedIn = box.read("isLoggedIn") ?? false; // default to false if not set
+
   runApp(
     GetMaterialApp(
       initialRoute: "/",
       getPages: routes.toList(),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: isLoggedIn ? Homescreen() : LoginScreen(), 
     ),
   );
-  
 }
